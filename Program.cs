43cda0c4358462313bab76a93553291ff8a33f34
Administrateur <Administrateur@DESKTOP-QUDI77S>
2020-12-10 12:49:56 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void change(ref int i)
+        {
+            i = i + 15;
+        }
+        static void Main(string[] args)
+        {
+
+            int val = 10;
+            change(ref val);
+            Console.WriteLine(val);
+            
+            
+         
+        }
+    }
+}
